d48a2dd6c916d2d2a88cea67f5f5be45edda2e30
Tomke Jósiak <tomek.josiak@gmail.com>
2017-04-25 16:53:35 +0200

    Initializing react project
    Signed-off-by: Tomke Jósiak <tomek.josiak@gmail.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+# Browser Neuroshima Client
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './app/main.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'app')
+	},
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules|bower_components)/,
+				use: [
+					{ loader: 'babel-loader' },
+					{ loader: 'eslint-loader' }
+				]
+			},
+			{
+				test: /\.scss$/,
+				exclude: /(node_modules|bower_components)/,
+				use: ExtractTextPlugin.extract({
+        			use: ["css-loader", "sass-loader"]
+      			})
+			}
+		]
+	},
+	plugins: [
+    	new DashboardPlugin(),
+		new ExtractTextPlugin({
+		    filename: "bundle.css",
+  		})
+	],
+	devServer: {
+	  contentBase: path.join(__dirname, "app"),
+	  historyApiFallback: true,
+	  port: 8080
+	}
+
+};
