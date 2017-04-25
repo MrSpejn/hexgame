83059198843e80be76d25ceba46d22cd64fea743
Tomke Jósiak <tomek.josiak@gmail.com>
2017-04-25 17:17:56 +0200

    Creating main component

--- app/main.js
+++ app/main.js
@@ -1,1 +1,13 @@
 import "./main.scss";
+/*eslint-disable no-unused-vars*/
+import React from "react";
+/*eslint-enable no-unused-vars*/
+import ReactDOM from "react-dom";
+
+import MainComponent from "./components/main.component.js";
+
+
+
+const mountPoint = document.querySelector(".mount-point");
+
+ReactDOM.render(<MainComponent />, mountPoint);
